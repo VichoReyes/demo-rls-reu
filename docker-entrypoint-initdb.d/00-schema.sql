2d0eee7828e7c2eeb891ec8dab7a268241076847
Vicente Reyes <vreyesvaldivieso@gmail.com>
2022-05-25 17:51:15 -0400

    este sí era el original

--- docker-entrypoint-initdb.d/00-schema.sql
+++ docker-entrypoint-initdb.d/00-schema.sql
@@ -7,53 +7,48 @@ create role web_logged nologin;
 grant web_logged to authenticator;
 
 
+create table organizations (
+    id integer primary key generated always as identity,
+    org_name text unique not null,
+    members_need_admin boolean not null default false
+);
+
 create table users (
     id integer primary key generated always as identity,
     username text unique not null
 );
 
-create table user_profiles (
-    user_id foreign key references users(id),
-    pic_url text unique,
-    descrip text not null default '',
-    is_private boolean
-)
-
--- Populate the table
-INSERT INTO users (id, username) VALUES (
-    (1, 'Alice'),
-    ()
+create table user_org (
+    id integer primary key generated always as identity,
+    organization_id int references organizations(id),
+    user_id int references users(id),
+    is_admin boolean not null default false
 );
-  ('admin','xxx',0,0,'Admin','111-222-3333',null,'/root','/bin/dash');
-INSERT INTO passwd VALUES
-  ('bob','xxx',1,1,'Bob','123-456-7890',null,'/home/bob','/bin/zsh');
-INSERT INTO passwd VALUES
-  ('alice','xxx',2,1,'Alice','098-765-4321',null,'/home/alice','/bin/zsh');
 
--- Be sure to enable row-level security on the table
-ALTER TABLE passwd ENABLE ROW LEVEL SECURITY;
+create table repositories (
+    id integer primary key generated always as identity,
+    organization_id int references organizations(id),
+    repo_name text not null,
+    unique (organization_id, repo_name)
+);
 
--- Create policies
--- Administrator can see all rows and add any rows
-CREATE POLICY admin_all ON passwd TO admin USING (true) WITH CHECK (true);
--- Normal users can view all rows
-CREATE POLICY all_view ON passwd FOR SELECT USING (true);
--- Normal users can update their own records, but
--- limit which shells a normal user is allowed to set
-CREATE POLICY user_mod ON passwd FOR UPDATE
-  USING (current_user = user_name)
-  WITH CHECK (
-    current_user = user_name AND
-    shell IN ('/bin/bash','/bin/sh','/bin/dash','/bin/zsh','/bin/tcsh')
-  );
+grant usage on schema public to web_anon;
+grant usage on schema public to web_logged;
 
--- Allow admin all normal rights
-GRANT SELECT, INSERT, UPDATE, DELETE ON passwd TO admin;
--- Users only get select access on public columns
-GRANT SELECT
-  (user_name, uid, gid, real_name, home_phone, extra_info, home_dir, shell)
-  ON passwd TO public;
--- Allow users to update certain columns
-GRANT UPDATE
-  (pwhash, real_name, home_phone, extra_info, shell)
-  ON passwd TO public;
+create table repositories (
+    id integer primary key generated always as identity,
+    organization_id int references organizations(id),
+    repo_name text not null,
+    unique (organization_id, repo_name)
+);
+
+create table documents (
+    id serial,
+    contents text
+);
+
+create table document_relation (
+    relation_name text, -- "owner", "editor" o "viewer"
+    document_id foreign key references documents(id),
+    user_id foreign key references users(id)
+);
